5a634fa74650eaaaad2b169f46308fc5adba5ad0
leirb4G <gabriel.bandeira19991@gmail.com>
2026-07-01 21:10:50 -0300

    js de arquivamento local

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,2 +1,23 @@
 //Tudo relacionado ao localStorage
 //Exemplo: Salvar usuários, Salvar time, Carregar favoritos.
+
+
+function salvarTime(nomeDoPokemon, idPokemon, itenPokemon) {
+    console.log("Nome do pokemon: " + nomeDoPokemon);
+    console.log("Id do pokemon: " + idPokemon);
+    console.log("Iten seguro:" + itenPokemon);
+    console.log("_--_--_--_--_--_--_--_--_--_--_--_");
+
+    let salvar = {
+        nome: nomeDoPokemon,
+        id: idPokemon,
+        iten : itenPokemon,
+        resultado: resultado
+    };
+
+    let salvarPokemon = JSON.parse(localStorage.getItem("dadosTime")) || [];
+    salvarPokemon.push(salvar);
+
+    localStorage.setItem("dadosTime", JSON.stringify(salvarPokemon));
+}
+
