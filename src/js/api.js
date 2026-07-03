a25bb37bf3d31d66710230f3fc7325e18e2419e6
leirb4G <gabriel.bandeira19991@gmail.com>
2026-07-02 21:20:38 -0300

    Registro de storage funcionando

--- src/js/api.js
+++ src/js/api.js
@@ -23,6 +23,7 @@ function buscaPokemon() {
         document.getElementById("tipo2").value = "";
 
         document.getElementById("nome").value = dados.name;
+        document.getElementById("id").value = dados.id;
         document.getElementById("imagem").value = dados.sprites.front_default;
         document.getElementById("tipo1").value = dados.types[0].type.name;
         document.getElementById("tipo2").value = dados.types[1].type.name;
@@ -33,7 +34,37 @@ const formPokemon = document.getElementById("nome");
 formPokemon.addEventListener('focusout', buscaPokemon);
 
 
+function buscaPokemonId() {
+    let pokemonDigitado = document.getElementById("nome").value;
+    
 
+    if (pokemonDigitado === "") {
+        alert("Digite um Pokémon");
+        return;
+    }
+
+    console.log("pokemonDigitado");
+
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonDigitado}`)
+    .then(function (resposta) {
+        return resposta.json();
+    })
+    .then(function(dados) {
+        if (dados.erro) {
+            alert("Pokémon não encontrado")
+            return;
+        }
+        document.getElementById("tipo1").value = "";
+        document.getElementById("tipo2").value = "";
+
+        document.getElementById("nome").value = dados.name;
+        document.getElementById("id").value = dados.id;
+        document.getElementById("imagem").value = dados.sprites.front_default;
+        document.getElementById("tipo1").value = dados.types[0].type.name;
+        document.getElementById("tipo2").value = dados.types[1].type.name;
+    })
+}
 
 /* 
 
--- src/js/storage.js
+++ src/js/storage.js
@@ -2,22 +2,30 @@
 //Exemplo: Salvar usuários, Salvar time, Carregar favoritos.
 
 
+
+
 function salvarTime(nomeDoPokemon, idPokemon, itenPokemon) {
-    console.log("Nome do pokemon: " + nomeDoPokemon);
-    console.log("Id do pokemon: " + idPokemon);
-    console.log("Iten seguro:" + itenPokemon);
+
+    let nomeSalvo = document.getElementById("nome").value;
+    let idSalvo = document.getElementById("id").value
+    let itenTeste = document.getElementById("item").value;
+
+    console.log("Nome do pokemon: " + nomeSalvo);
+    console.log("Id do pokemon: " + idSalvo);
+    console.log("Iten seguro:" + itenTeste);
     console.log("_--_--_--_--_--_--_--_--_--_--_--_");
 
     let salvar = {
-        nome: nomeDoPokemon,
-        id: idPokemon,
-        iten : itenPokemon,
-        resultado: resultado
+        nome: nomeSalvo,
+        id: idSalvo,
+        iten : itenTeste,
+        
     };
-
+    
     let salvarPokemon = JSON.parse(localStorage.getItem("dadosTime")) || [];
     salvarPokemon.push(salvar);
 
     localStorage.setItem("dadosTime", JSON.stringify(salvarPokemon));
+    
 }
 
